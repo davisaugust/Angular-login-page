ffb3b068fadba8c762efca3e3654841c9f6c593b
Davi Augusto <davisaugust004@Gmail.com>
2025-06-23 00:59:13 -0300

    feat: verification function and token generation with spring boot

--- src/app/services/login-service.ts
+++ src/app/services/login-service.ts
@@ -6,12 +6,23 @@ import { tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
 export class LoginService {
+  apiUrl: string = "http://localhost:8080/auth"
 
   constructor(private httpClient: HttpClient) { }
 
   login(email: string, password: string){
-    return this.httpClient.post<LoginResponse>("login", {email, password}).pipe(
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+
+  signup(name: string, email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
